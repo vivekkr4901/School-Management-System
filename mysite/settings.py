3006b2fd34091b4926ffe0f8a3beb13da6c6cdae
vivekkr4901 <kumharvivek6791@gmail.com>
2024-05-28 23:04:17 +0530

    ui improve

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,11 +75,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME':'mysitedb',
+'USER':'postgres',
+'PASSWORD':'p123@sqd',
+'HOST':'localhost',
+'PORT':5432
 }
+}
+
+
 
 
 # Password validation
